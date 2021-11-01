5d5da3fe30cfd688dca1d262312ff3a2253ea4d3
Anastasia <stepashkaa2602@gmail.com>
2021-11-02 02:21:11 +0300

    реализация CRU для товаров

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,77 +1,54 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.example.demo.form.NomenclatureForm;
-import com.example.demo.model.Nomenclature;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Nomenclature;
+import com.example.demo.service.NomenclatureService;
 
 @Controller
 public class MainController {
 
-    private static List<Nomenclature> nomenclatures = new ArrayList<Nomenclature>();
+    @Autowired
+    private NomenclatureService nomenclatureService;
 
-    static {
-        nomenclatures.add(new Nomenclature("0102", "Лего бионикл", 150));
-        nomenclatures.add(new Nomenclature("0103", "Лего нинзяга", 200));
-    }
-
-    // Вводится (inject) из application.properties.
-    @Value("${welcome.message}")
-    private String message;
-
-    @Value("${error.message}")
-    private String errorMessage;
-
-    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    @GetMapping(value = { "/", "/index" })
     public String index(Model model) {
-
-        model.addAttribute("message", message);
-
         return "index";
     }
 
-    @RequestMapping(value = { "/nomenclatureList" }, method = RequestMethod.GET)
+    @GetMapping("/nomenclaturesList")
     public String nomenclatureList(Model model) {
 
-        model.addAttribute("nomenclatures", nomenclatures);
+        model.addAttribute("nomenclaturesList", nomenclatureService.getAllNomenclatures());
 
-        return "nomenclatureList";
+        return "nomenclaturesList";
     }
 
-    @RequestMapping(value = { "/addNomenclature" }, method = RequestMethod.GET)
+    @GetMapping("/addNomenclature")
     public String showAddNomenclaturePage(Model model) {
-
-        NomenclatureForm nomenclatureForm = new NomenclatureForm();
-        model.addAttribute("nomenclatureForm", nomenclatureForm);
+        Nomenclature nomenclature = new Nomenclature();
+        model.addAttribute("nomenclature", nomenclature);
 
         return "addNomenclature";
     }
 
-    @RequestMapping(value = { "/addNomenclature" }, method = RequestMethod.POST)
-    public String saveNomenclature(Model model, //
-                             @ModelAttribute("nomenclatureForm") NomenclatureForm nomenclatureForm) {
+    @PostMapping("/saveNomenclature")
+    public String saveNomenclature(@ModelAttribute("nomenclature") Nomenclature nomenclature) {
+        nomenclatureService.saveNomenclature(nomenclature);
+        return "redirect:/nomenclaturesList";
+    }
 
-        String code = nomenclatureForm.getCode();
-        String description = nomenclatureForm.getDescription();
-        int price = nomenclatureForm.getPrice();
-
-        if (code != null && code.length() > 0 //
-                && description != null && description.length() > 0 && price != 0) {
-            Nomenclature newNomenclature = new Nomenclature(code, description, price);
-            nomenclatures.add(newNomenclature);
-
-            return "redirect:/nomenclatureList";
-        }
-
-        model.addAttribute("errorMessage", errorMessage);
-        return "addNomenclature";
+    @GetMapping("/updateNomenclature/{id}")
+    public String updateNomenclature(@PathVariable(value="id") int id, Model model) {
+        Nomenclature nomenclature = nomenclatureService.getNomenclatureById(id);
+        model.addAttribute("nomenclature", nomenclature);
+        return "updateNomenclature";
     }
 
 }
